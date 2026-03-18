d42a0b94b445ecff75983aaef0d6f71b94f24edb
mayellibilistki-stack <mayelli.bilistki@escola.pr.gov.br>
2026-03-18 08:49:25 -0300

    Update desafio 3.py

--- desafio 3.py
+++ desafio 3.py
@@ -1,1 +1,7 @@
+# 1. Cria uma lista com 5 nomes
+nomes = ["Parisotto", "Fedechen", "Mayelli", "Gustavo", "Henz"]
 
+# 2. Depois utiliza o laço 'for' para percorrer a lista
+print("Lista de nomes:")
+for nome in nomes:
+    print(nome)
