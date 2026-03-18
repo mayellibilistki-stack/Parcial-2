88507443e5a44c3129cb9f0ad9fae7d70f18eda4
mayellibilistki-stack <mayelli.bilistki@escola.pr.gov.br>
2026-03-18 08:42:47 -0300

    Update desafio 2.py

--- desafio 2.py
+++ desafio 2.py
@@ -1,1 +1,11 @@
 
+# Programa que verifica se um número é par
+
+# Pergunta ao usuário que digite um número
+numero = int(input("Digite um número: "))
+
+# Verifica se o número é par
+if numero % 2 == 0:
+    print(f"O número {numero} é par.")
+else:
+    print(f"O número {numero} é ímpar.")
