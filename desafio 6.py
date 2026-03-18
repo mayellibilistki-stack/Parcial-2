3936e8838f762f74dc69a30c44b645d597d87c84
mayellibilistki-stack <mayelli.bilistki@escola.pr.gov.br>
2026-03-18 10:36:57 -0300

    Update desafio 6.py

--- desafio 6.py
+++ desafio 6.py
@@ -10,3 +10,14 @@ segundos_finais = resto_segundos % 60
 # O final é a exibição do resultado
 print(f"{total_segundos} segundos equivalem a:")
 print(f"{horas}h {minutos}min {segundos_finais}s")
+
+# Vai ser a entrada do usuário
+h = int(input("Horas: "))
+m = int(input("Minutos: "))
+s = int(input("Segundos: "))
+
+# É realizado o cálculo: 1h = 3600s | 1min = 60s
+resultado_segundos = (h * 3600) + (m * 60) + s
+
+# É a parte da exibição do resultado
+print(f"O total convertido é de {resultado_segundos} segundos.")
