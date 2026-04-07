94886217daa45b5d4d39a2431a54ed24f2e1594d
mayellibilistki-stack <mayelli.bilistki@escola.pr.gov.br>
2026-04-07 09:41:02 -0300

    Update desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,8 +1,6 @@
-def soma(a, b):
-    return a + b
+
 # Aqui ele vai solicitar que você digite o numero que deseja somar
 num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-#Resultado da soma 
-resultado = soma(num1, num2)
-print(f"A soma de {num1} e {num2} é: {resultado}")
+num2 = float(input("Digite o segundo número: "))  
+soma = num1 + num2
+print(soma)
