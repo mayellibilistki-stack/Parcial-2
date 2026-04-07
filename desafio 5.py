38ce04459074a09cd642e154ad013689567af806
mayellibilistki-stack <mayelli.bilistki@escola.pr.gov.br>
2026-04-07 09:20:23 -0300

    Update desafio 5.py

--- desafio 5.py
+++ desafio 5.py
@@ -1,20 +1,12 @@
-# Primeira função é para calcular a área de um triângulo.
-def calcular_area_triangulo(base, altura):
-    return (base * altura) / 2
-
-# Essa é a função pincipal de todas
-def main():
-    print("Cálcular a área de um triângulo")
-    
-    # Após tudo isso, é preciso solicitar a base e altura do triângulo
-    base = float(input("Digite aqui qual é o valor da base do triângulo: "))
-    altura = float(input("Digite o valor da altura do triângulo que você deseja medir: "))
+#Base é a variavel para fazer a pergunta da base do triângulo
+#Altura é a variavel para fazer a pergunta da altura do triângulo
+base = float(input("Qual a base do triângulo: "))
+altura = float(input("Qual altura do triângulo: "))
     
     # Depois, vem a forma que vai ser usado para calcular a área
-    area = calcular_area_triangulo(base, altura)
+area = (base * altura) / 2
     
     # E por fim mostra o resultado final
-    print(f"A área do triângulo com base {base} e altura {altura} é: {area}")
+print(area)
 
-# Vai chamar a função principal
-main()
+
